376e21f6f26adc7909f1bbd0b5406cff7177d5e1
Dharshini310 <dharshini3102k3@gmail.com>
2025-05-05 19:18:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import diwali from "./diwali.jpeg";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Home">
+      <div className="name">GUDDATI DHARSHINI<br/>
+      <text className="me">UI/UX Designer,Frontend Developer<br/>
+      <button><a href="https://github.com/Dharshini310" className="a">Explore My Work</a></button></text></div>
+        <img src={diwali} alt="loading.." className="img" />
+      </div><br/>
+      <div className="Home1">
+      <div className="name1">About Me
+      </div>
+      <p>Hi, I'm Dharshini — a creative UI/UX Designer and Frontend Developer with a passion for crafting seamless digital experiences. I specialize in translating ideas into visually appealing and user-friendly interfaces using HTML, CSS, JavaScript, React and design tools like Figma.
+My approach blends design thinking with clean, responsive code, ensuring that every project not only looks great but functions flawlessly across devices. With additional skills in Python and SQL, I bring a well-rounded perspective to both design and development.
+I'm driven by curiosity, collaboration, and the desire to make technology more intuitive and engaging for users.</p>
+      </div>
+      <br/>
+      <div className="Home1">
+      <div className="name1">Skills and Projects
+      </div>
+      <p className="skills"><span>Skills</span><br/>
+      Frontend Skills: HTML,CSS,JavaScript,React<br/>
+Programming Skills: Python,SQL<br/>
+UI/UX Design: Figma-Wireframing,Prototyping,Visual Design<br/>
+Tools and Technologies: VS Code,GitHub,Responsive Design,BrowserDev Tools<br/>
+<span>Projects</span><br/><span>Figma Projects</span><br/>
+<a href="https://drive.google.com/file/d/1Dcz-JZkibjDaHk80x-UHkQORUBWExeub/view">Ram Navami Wishes Design</a>&nbsp;&nbsp;<a href="https://drive.google.com/file/d/1Om9pKEUNcXUwJNv8YtJQAp5sfYOyEDDS/view">E-learning Sign In Design</a>&nbsp;&nbsp;<a href="https://drive.google.com/file/d/1SgGe6Y5LQCq39GPLk5xTYOMY4jhA_mHw/view">MindEase Design</a>&nbsp;&nbsp;<a href="https://drive.google.com/file/d/1wi567VzlJP6KzgDnkhhuIUnRSWRMTbWS/view">My Website Design</a><br/>
+<span>Frontend Projects</span><br/>
+<a href="https://career-vista-hub.vercel.app/">CareerVista Hub</a>&nbsp;&nbsp;<a href="https://dharshini310.github.io/XO_Challenge/">Tic Tac Toe</a>&nbsp;&nbsp;<a href="https://dharshini310.github.io/QR-Code-Generator/">QR Code Generator</a>&nbsp;&nbsp;<a href="https://age-calculator-mocha-beta.vercel.app/">Age Calculator</a><br/>
+</p>
+      </div>
+      <p>For Contact: dharshini3102k3@gmail.com &nbsp;&nbsp;&nbsp; 6281168212 &nbsp;&nbsp;&nbsp;  <a href="https://www.linkedin.com/in/guddati-dharshini-5ab442253/">LinkedIn</a> &nbsp;&nbsp;&nbsp; <a href="https://github.com/Dharshini310">GitHub</a><br/>
+      <span>Open to freelance, internships, or collaboration opportunities!</span></p>
     </div>
   );
 }
